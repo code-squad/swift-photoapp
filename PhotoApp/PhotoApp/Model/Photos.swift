a0d36bb415e60b6c3f8124b45020612ba11f9b75
undervineg <undervineg@gmail.com>
2018-04-17 11:52:17 +0900

    새 파일 추가

---
+++ PhotoApp/PhotoApp/Model/Photos.swift
@@ -0,0 +1,9 @@
+//
+//  Photos.swift
+//  PhotoApp
+//
+//  Created by 심 승민 on 2018. 4. 16..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import Foundation
---
+++ PhotoApp/PhotoApp/View/PhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCell.swift
+//  PhotoApp
+//
+//  Created by 심 승민 on 2018. 4. 16..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class PhotoCell: UICollectionViewCell, Reusable {
+    @IBOutlet weak var photoImageView: UIImageView!
+}
