9ecd197f666a15aedf9227bffcab049bc84493ae
undervineg <undervineg@gmail.com>
2018-04-17 18:10:37 +0900

    [추가] 사진 update 메소드 추가

--- PhotoApp/PhotoApp/Model/Photos.swift
+++ PhotoApp/PhotoApp/Model/Photos.swift
@@ -8,23 +8,24 @@
 
 import Photos
 
-struct Photos: Sequence {
-    private var photoAssets = PHFetchResult<PHAsset>()
+class Photos: Sequence {
+    private(set) var photoAssets = PHFetchResult<PHAsset>()
     let start: Int = 0
 
     init() {
         self.photoAssets = self.fetchAllPhotosFromLibrary()
     }
 
-    func makeIterator() -> IteratorForPHFetchResult<PHAsset> {
-        return IteratorForPHFetchResult(self.photoAssets)
+    public func makeIterator() -> ClassIterator<PHAsset> {
+        return ClassIterator.init(self.photoAssets)
     }
+
 }
 
 extension Photos {
     private func fetchAllPhotosFromLibrary() -> PHFetchResult<PHAsset> {
         let options = PHFetchOptions()
-        options.sortDescriptors = [NSSortDescriptor.init(key: "creationDate", ascending: true)]
+        options.sortDescriptors = [NSSortDescriptor.init(key: "creationDate", ascending: false)]
         return PHAsset.fetchAssets(with: options)
     }
 
@@ -35,20 +36,8 @@ extension Photos {
     var count: Int {
         return photoAssets.count
     }
-}
 
-struct IteratorForPHFetchResult<Type: NSObject>: IteratorProtocol {
-    typealias Element = Type
-    private let elements: PHFetchResult<Element>
-    private var nextIndex: Int
-    init(_ elements: PHFetchResult<Element>) {
-        self.nextIndex = 0
-        self.elements = elements
-    }
-
-    mutating func next() -> Element? {
-        defer { self.nextIndex += 1 }
-        guard self.nextIndex < self.elements.count else { return nil }
-        return self.elements[self.nextIndex]
+    func updateAssets(with changedAssets: PHFetchResult<PHAsset>) {
+        self.photoAssets = changedAssets
     }
 }
