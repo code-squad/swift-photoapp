f75d257074eea0c8a83f1644b2e4b89852fdee93
undervineg <undervineg@gmail.com>
2018-04-18 00:40:06 +0900

    [수정] 이미지뷰의 오토레이아웃 제약사항을 코드로 변경. contentMode를 aspect fill 로 변경하여 이미지뷰를 꽉 채우도록 함

--- PhotoApp/PhotoApp/View/PhotoCell.swift
+++ PhotoApp/PhotoApp/View/PhotoCell.swift
@@ -9,5 +9,14 @@
 import UIKit
 
 class PhotoCell: UICollectionViewCell, Reusable {
-    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoImageView: UIImageView! {
+        didSet {
+            photoImageView.translatesAutoresizingMaskIntoConstraints = false
+            photoImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+            photoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+            photoImageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+            photoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+            photoImageView.contentMode = .scaleAspectFill
+        }
+    }
 }
