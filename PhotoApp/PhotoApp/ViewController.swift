9a3473ea62e19bf5d2f49988c8ba5ab8cfce5017
undervineg <undervineg@gmail.com>
2018-04-17 11:57:50 +0900

    [추가] photo 데이터 추가

--- PhotoApp/PhotoApp/ViewController.swift
+++ PhotoApp/PhotoApp/ViewController.swift
@@ -15,24 +15,21 @@ class ViewController: UIViewController, UICollectionViewDataSource {
         }
     }
 
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 40
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = UIColor.random
-        return cell
-    }
+    var photos: Photos!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.photos = Photos()
     }
 
-}
-
-extension UIColor {
-    static var random: UIColor {
-        return UIColor.init(red: CGFloat(drand48()), green: CGFloat(drand48()), blue: CGFloat(drand48()), alpha: 1)
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
     }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCell.id, for: indexPath) as! PhotoCell
+        let photo = photos.at(indexPath.item)
+        return cell
+    }
+
 }
