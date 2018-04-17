b6cee758370cbc3aa4d80ad0fa0f550527aec7a6
undervineg <undervineg@gmail.com>
2018-04-18 00:40:56 +0900

    [추가] UICollectionViewDelegateFlowLayout를 채택하여 코드로 셀 크기 지정

--- PhotoApp/PhotoApp/ViewController.swift
+++ PhotoApp/PhotoApp/ViewController.swift
@@ -9,13 +9,13 @@
 import UIKit
 import Photos
 
-class ViewController: UIViewController, UICollectionViewDataSource {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     @IBOutlet weak var collectionView: UICollectionView! {
         didSet {
             collectionView.dataSource = self
+            collectionView.delegate = self
         }
     }
-    private var photos: Photos!
     private var photoService: PhotoService! {
         didSet {
             photoService.delegate = self
@@ -24,12 +24,11 @@ class ViewController: UIViewController, UICollectionViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.photos = Photos()
-        self.photoService = PhotoService(photos: photos)
+        self.photoService = PhotoService()
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return photos.count
+        return photoService.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -40,26 +39,30 @@ class ViewController: UIViewController, UICollectionViewDataSource {
         return cell
     }
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: ViewConfig.itemWidth, height: ViewConfig.itemHeight)
+    }
+
 }
 
 extension ViewController: UpdateCollectionViewDelegate {
     func updateCollectionView(_ changes: PHFetchResultChangeDetails<PHAsset>) {
-        if changes.hasIncrementalChanges {
-            self.collectionView.performBatchUpdates({
-                if let insertedIndexes = changes.insertedIndexes, insertedIndexes.count > 0 {
-                    self.collectionView.insertItems(at: insertedIndexes.compactMap { IndexPath(index: $0) })
-                }
-                if let deletedIndexes = changes.removedIndexes, deletedIndexes.count > 0 {
-                    self.collectionView.deleteItems(at: deletedIndexes.compactMap { IndexPath(index: $0) })
-                }
-                if let changedIndexes = changes.changedIndexes, changedIndexes.count > 0 {
-                    self.collectionView.reloadItems(at: changedIndexes.compactMap { IndexPath(index: $0) })
-                }
-                changes.enumerateMoves { self.collectionView.moveItem(at: IndexPath(index: $0), to: IndexPath(index: $1)) }
-            })
-        } else {
-            self.collectionView.reloadData()
-        }
+        changes.hasIncrementalChanges ? updateChangedItems(changes) : self.collectionView.reloadData()
+    }
+
+    private func updateChangedItems(_ changes: PHFetchResultChangeDetails<PHAsset>) {
+        self.collectionView.performBatchUpdates({
+            if let insertedIndexes = changes.insertedIndexes, insertedIndexes.count > 0 {
+                self.collectionView.insertItems(at: insertedIndexes.compactMap { IndexPath(index: $0) })
+            }
+            if let deletedIndexes = changes.removedIndexes, deletedIndexes.count > 0 {
+                self.collectionView.deleteItems(at: deletedIndexes.compactMap { IndexPath(index: $0) })
+            }
+            if let changedIndexes = changes.changedIndexes, changedIndexes.count > 0 {
+                self.collectionView.reloadItems(at: changedIndexes.compactMap { IndexPath(index: $0) })
+            }
+            changes.enumerateMoves { self.collectionView.moveItem(at: IndexPath(index: $0), to: IndexPath(index: $1)) }
+        })
     }
 
 }
