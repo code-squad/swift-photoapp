b793331fbf212cecfd4fa3c5f2175f0c9ff63f0d
undervineg <undervineg@gmail.com>
2018-04-18 00:10:28 +0900

    새 파일 추가

---
+++ PhotoApp/PhotoApp/Constants/CollectionView+Config.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionView+Config.swift
+//  PhotoApp
+//
+//  Created by 심 승민 on 2018. 4. 17..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import UIKit
+
+struct ViewConfig {
+    static let itemWidth: CGFloat = 100
+    static let itemHeight: CGFloat = 100
+}
