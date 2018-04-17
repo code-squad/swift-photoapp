1f53fee19da1fb29947113a2951d8bd4a57aebe6
undervineg <undervineg@gmail.com>
2018-04-17 11:55:53 +0900

    [추가] 불러온 사진 데이터를 저장할 구조체 추가
    + 내부 photoAssets 데이터 접근을 랩핑하기 위해 Sequence 채택
    + 앨범의 모든 사진을 creationDate 순으로 가져오는 메소드 추가

--- PhotoApp/PhotoApp/Model/Photos.swift
+++ PhotoApp/PhotoApp/Model/Photos.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2018년 심 승민. All rights reserved.
 //
 
-import Foundation
+import Photos
+
+struct Photos: Sequence {
+    private var photoAssets = PHFetchResult<PHAsset>()
+    let start: Int = 0
+
+    init() {
+        self.photoAssets = self.fetchAllPhotosFromLibrary()
+    }
+
+    func makeIterator() -> IteratorForPHFetchResult<PHAsset> {
+        return IteratorForPHFetchResult(self.photoAssets)
+    }
+}
+
+extension Photos {
+    private func fetchAllPhotosFromLibrary() -> PHFetchResult<PHAsset> {
+        let options = PHFetchOptions()
+        options.sortDescriptors = [NSSortDescriptor.init(key: "creationDate", ascending: true)]
+        return PHAsset.fetchAssets(with: options)
+    }
+
+    func at(_ index: Int) -> PHAsset {
+        return photoAssets.object(at: index)
+    }
+
+    var count: Int {
+        return photoAssets.count
+    }
+}
+
+struct IteratorForPHFetchResult<Type: NSObject>: IteratorProtocol {
+    typealias Element = Type
+    private let elements: PHFetchResult<Element>
+    private var nextIndex: Int
+    init(_ elements: PHFetchResult<Element>) {
+        self.nextIndex = 0
+        self.elements = elements
+    }
+
+    mutating func next() -> Element? {
+        defer { self.nextIndex += 1 }
+        guard self.nextIndex < self.elements.count else { return nil }
+        return self.elements[self.nextIndex]
+    }
+}
