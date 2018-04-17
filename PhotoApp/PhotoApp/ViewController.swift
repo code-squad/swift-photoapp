cd37abf646e49883b7cf5d2d138eb0a1a3c91ca0
undervineg <undervineg@gmail.com>
2018-04-17 12:12:22 +0900

    [추가] 셀에 이미지 표시

--- PhotoApp/PhotoApp/ViewController.swift
+++ PhotoApp/PhotoApp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController, UICollectionViewDataSource {
     @IBOutlet weak var collectionView: UICollectionView! {
@@ -14,8 +15,12 @@ class ViewController: UIViewController, UICollectionViewDataSource {
             collectionView.dataSource = self
         }
     }
-
-    var photos: Photos!
+    private let imageManager = PHCachingImageManager()
+    private var photos: Photos! {
+        didSet {
+            collectionView.reloadData()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +34,10 @@ class ViewController: UIViewController, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCell.id, for: indexPath) as! PhotoCell
         let photo = photos.at(indexPath.item)
+        imageManager.requestImage(for: photo,
+                                  targetSize: CGSize(width: 100, height: 100),
+                                  contentMode: .aspectFill,
+                                  options: nil) { image, _ in cell.photoImageView.image = image }
         return cell
     }
 
