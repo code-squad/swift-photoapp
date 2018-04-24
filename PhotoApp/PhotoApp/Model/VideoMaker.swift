b48e286d59b5f198a3b0a51501c56f55ac4942fa
undervineg <undervineg@gmail.com>
2018-04-24 14:41:25 +0900

    새 파일 추가

---
+++ PhotoApp/PhotoApp/Model/VideoMaker.swift
@@ -0,0 +1,15 @@
+//
+//  AVVideoMaker.swift
+//  PhotoApp
+//
+//  Created by 심 승민 on 2018. 4. 21..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import Photos
+
+class AVVideoMaker {
+    func makeVideo(from images: [UIImage]) {
+        
+    }
+}
