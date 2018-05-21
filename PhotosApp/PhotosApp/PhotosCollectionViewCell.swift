b703dc0efea5e74404ed1da4670f3bd9edcc8ef4
ChocOZerO <nayalth@gmail.com>
2018-05-21 16:56:34 +0900

    [Add]Custom CollectionView Cell 추가 #5

---
+++ PhotosApp/PhotosApp/PhotosCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotosApp
+//
+//  Created by TaeHyeonLee on 2018. 5. 21..
+//  Copyright © 2018년 ChocOZerO. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
