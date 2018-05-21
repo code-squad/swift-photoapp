cd127f520ffc52b5b2b2d0c844d5977a6bdec01e
ChocOZerO <nayalth@gmail.com>
2018-05-21 12:11:14 +0900

    [Init]PhotosApp #1

---
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotosApp
+//
+//  Created by TaeHyeonLee on 2018. 5. 21..
+//  Copyright © 2018년 ChocOZerO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
