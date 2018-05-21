b5caaa7df1676f55df6c9f1c5141c85c76c37725
ChocOZerO <nayalth@gmail.com>
2018-05-21 14:49:44 +0900

    [Add]Cell에 랜덤 색상 입히기 #2

--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collectionView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+}
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 40
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photo", for: indexPath)
+        let redElement = getRandomColor()
+        let greenElement = getRandomColor()
+        let blueElement = getRandomColor()
+        let color = UIColor.init(red: redElement, green: greenElement, blue: blueElement, alpha: 1)
+        cell.backgroundColor = color
+        return cell
+    }
+
+    private func getRandomColor() -> CGFloat {
+        return  CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
 }
 
