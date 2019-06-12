af529c0352097a1f56d8cd85e26b0f379b4395c9
Hngfu <djsl1018@naver.com>
2019-06-12 18:12:18 +0900

    [ViewController] UICollectionViewDataSource, UICollectionViewDelegateFlowLayout 채택, 준수

--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -15,8 +15,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        randomColorCollectionView.delegate = self
+        randomColorCollectionView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 40
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RandomColorCollectionViewCell.identifier,
+                                                      for: indexPath)
+
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: 80, height: 80)
+    }
+}
