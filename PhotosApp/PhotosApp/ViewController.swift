b26995ca712440abbcf095e68a2d396cd0aa981e
Hngfu <djsl1018@naver.com>
2019-06-12 17:56:02 +0900

    CollectionView 추가 및 ViewController에 연결

--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var randomColorCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
