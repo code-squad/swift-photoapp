03bae918cbcb4edd2ca74462ac39c25ffe085ac3
Hngfu <djsl1018@naver.com>
2019-06-13 18:19:02 +0900

    [Info.plist] 사진 접근 권한 요청 추가

---
+++ PhotosApp/PhotosApp/PhotosCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotosApp
+//
+//  Created by 조재흥 on 19. 6. 13..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+}
