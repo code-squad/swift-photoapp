24b7fcaed4cfc9becb7392b0c4e98444b161940f
Hngfu <djsl1018@naver.com>
2019-06-13 18:19:02 +0900

    [Info.plist] 사진 접근 권한 요청 추가

---
+++ PhotosApp/PhotosApp/Configuration.swift
@@ -0,0 +1,9 @@
+//
+//  Configuration.swift
+//  PhotosApp
+//
+//  Created by 조재흥 on 19. 6. 13..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import Foundation
---
+++ PhotosApp/PhotosApp/PhotoConverter.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoConverter.swift
+//  PhotosApp
+//
+//  Created by 조재흥 on 19. 6. 13..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import Foundation
