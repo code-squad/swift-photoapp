e1a9cead4ba09783a2c03ae08fe64ec78495029c
Hngfu <djsl1018@naver.com>
2019-06-14 02:01:16 +0900

    [ViewController] photoAssets property 추가

--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -12,8 +12,8 @@ import Photos
 class ViewController: UIViewController {
 
     @IBOutlet weak var photosCollectionView: UICollectionView!
-    
-    private let photos = PhotoAssets()
+
+    private let photoAssets = PHAsset.fetchAssets(with: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,14 +26,14 @@ class ViewController: UIViewController {
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return photos.count()
+        return photoAssets.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotosCollectionViewCell.identifier,
                                                       for: indexPath)
-        guard let photoCell = cell as? PhotosCollectionViewCell,
-            let photoAsset = photos[indexPath.item] else { return cell }
+        guard let photoCell = cell as? PhotosCollectionViewCell else { return cell }
+        let photoAsset = photoAssets.object(at: indexPath.item)
         let converter = PhotoConverter()
         converter.convert(with: photoAsset) { (image) in
             photoCell.photoImageView.image = image
@@ -52,6 +52,6 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
 
 extension ViewController: PHPhotoLibraryChangeObserver {
     func photoLibraryDidChange(_ changeInstance: PHChange) {
-        
+        let resultChangeDetails = changeInstance.changeDetails(for: photoAssets)
     }
 }
