37b89e02e816b20b195a992c6a6489f75be38fcb
Hngfu <djsl1018@naver.com>
2019-06-13 18:22:09 +0900

    [PhotosCollectionViewCell] 추가

--- PhotosApp/PhotosApp/PhotosCollectionViewCell.swift
+++ PhotosApp/PhotosApp/PhotosCollectionViewCell.swift
@@ -10,4 +10,15 @@ import UIKit
 
 class PhotosCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    static let identifier = "photosCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 }
