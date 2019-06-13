16ada9ecef2f51aed9952756e0b116a754e83e95
Hngfu <djsl1018@naver.com>
2019-06-13 18:28:56 +0900

    [ViewController] 셀에 사진을 넣어 출력하도록 수정

--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -12,31 +12,38 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var randomColorCollectionView: UICollectionView!
     
+    private let photos = PhotoAssets()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         randomColorCollectionView.delegate = self
         randomColorCollectionView.dataSource = self
     }
-
-
 }
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 40
+        return photos.count()
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RandomColorCollectionViewCell.identifier,
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotosCollectionViewCell.identifier,
                                                       for: indexPath)
+        guard let photoCell = cell as? PhotosCollectionViewCell,
+            let photoAsset = photos[indexPath.item] else { return cell }
+        let converter = PhotoConverter()
+        converter.convert(with: photoAsset) { (image) in
+            photoCell.photoImageView.image = image
+        }
 
-        return cell
+        return photoCell
     }
 }
 
 extension ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return .init(width: 80, height: 80)
+        return .init(width: Configuration.Image.width,
+                     height: Configuration.Image.height)
     }
 }
