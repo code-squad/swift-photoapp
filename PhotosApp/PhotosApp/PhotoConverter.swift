06ac2886f498036633436ee72683e7ce60104a30
Hngfu <djsl1018@naver.com>
2019-06-13 18:26:29 +0900

    [PhotoConverter] 추가

--- PhotosApp/PhotosApp/PhotoConverter.swift
+++ PhotosApp/PhotosApp/PhotoConverter.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+import Photos
+
+struct PhotoConverter {
+    func convert(with asset: PHAsset, completion: @escaping (UIImage?) -> Void) {
+        let manager = PHImageManager.default()
+        manager.requestImage(for: asset,
+                             targetSize: .init(width: Configuration.Image.width,
+                                               height: Configuration.Image.height),
+                             contentMode: .aspectFill,
+                             options: nil) { (image, _) in
+                                completion(image)
+        }
+    }
+}
