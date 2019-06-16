1d2950a6a22ed428a1ac417c62ffd02fe05260ea
Hngfu <djsl1018@naver.com>
2019-06-16 16:48:23 +0900

    [ViewController] photoLibraryDidChange 구현

--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var photosCollectionView: UICollectionView!
 
-    private let photoAssets = PHAsset.fetchAssets(with: nil)
+    private var photoAssets = PHAsset.fetchAssets(with: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,6 +52,35 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
 
 extension ViewController: PHPhotoLibraryChangeObserver {
     func photoLibraryDidChange(_ changeInstance: PHChange) {
-        let resultChangeDetails = changeInstance.changeDetails(for: photoAssets)
+        guard let changes = changeInstance.changeDetails(for: photoAssets) else { return }
+        photoAssets = changes.fetchResultAfterChanges
+        
+        DispatchQueue.main.async {
+            if changes.hasIncrementalChanges {
+                self.update(collectionView: self.photosCollectionView,
+                            with: changes)
+            } else {
+                self.photosCollectionView.reloadData()
+            }
+        }
+    }
+    
+    private func update(collectionView: UICollectionView, with changes: PHFetchResultChangeDetails<PHAsset>) {
+        collectionView.performBatchUpdates({
+            if let removed = changes.removedIndexes, removed.count > 0 {
+                collectionView.deleteItems(at: removed.map { IndexPath(item: $0, section: 0)})
+            }
+            if let inserted = changes.insertedIndexes, inserted.count > 0 {
+                collectionView.insertItems(at: inserted.map { IndexPath(item: $0, section:0) })
+            }
+            if let changed = changes.changedIndexes, changed.count > 0 {
+                collectionView.reloadItems(at: changed.map { IndexPath(item: $0, section:0) })
+            }
+            changes.enumerateMoves { fromIndex, toIndex in
+                collectionView.moveItem(at: IndexPath(item: fromIndex, section: 0),
+                                        to: IndexPath(item: toIndex, section: 0))
+            }
+        })
     }
 }
+
