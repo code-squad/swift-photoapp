73b293f9612e484817904dd217986fa97bcf462a
Hngfu <djsl1018@naver.com>
2019-06-17 17:56:11 +0900

    PhotoManager가 PHPhotoLibraryChangeObserver을 채택, 준수하도록 핢

--- PhotosApp/PhotosApp/PhotoManager.swift
+++ PhotosApp/PhotosApp/PhotoManager.swift
@@ -10,22 +10,15 @@ import Foundation
 import Photos
 import PhotosUI
 
-class PhotoManager {
+class PhotoManager: NSObject {
     private var photoAssets = PHAsset.fetchAssets(with: nil)
     private let imageManager = PHCachingImageManager()
     private let thumbnailSize = CGSize(width: Configuration.Image.width,
                                        height: Configuration.Image.height)
     
-    func register(observer: PHPhotoLibraryChangeObserver) {
-        PHPhotoLibrary.shared().register(observer)
-    }
-    
-    func assets() -> PHFetchResult<PHAsset> {
-        return photoAssets
-    }
-    
-    func change(assets: PHFetchResult<PHAsset>) {
-        photoAssets = assets
+    override init() {
+        super.init()
+        PHPhotoLibrary.shared().register(self)
     }
     
     func count() -> Int {
@@ -58,37 +51,54 @@ class PhotoManager {
     }
 }
 
-extension ViewController: PHPhotoLibraryChangeObserver {
+extension PhotoManager: PHPhotoLibraryChangeObserver {
     func photoLibraryDidChange(_ changeInstance: PHChange) {
-        
-        guard let changes = changeInstance.changeDetails(for: self.photoManager.assets()) else { return }
-        self.photoManager.change(assets: changes.fetchResultAfterChanges)
-        
-        DispatchQueue.main.async {
-            if changes.hasIncrementalChanges {
-                self.update(collectionView: self.photosCollectionView,
-                            with: changes)
-            } else {
-                self.photosCollectionView.reloadData()
-            }
+        guard let changes = changeInstance.changeDetails(for: photoAssets) else { return }
+        photoAssets = changes.fetchResultAfterChanges
+        if changes.hasIncrementalChanges {
+            self.update(with: changes)
+        } else {
+            NotificationCenter.default.post(name: .notIncrementalChanges,
+                                            object: self)
         }
     }
     
-    private func update(collectionView: UICollectionView, with changes: PHFetchResultChangeDetails<PHAsset>) {
-        collectionView.performBatchUpdates({
-            if let removed = changes.removedIndexes, removed.count > 0 {
-                collectionView.deleteItems(at: removed.map { IndexPath(item: $0, section: 0)})
-            }
-            if let inserted = changes.insertedIndexes, inserted.count > 0 {
-                collectionView.insertItems(at: inserted.map { IndexPath(item: $0, section:0) })
-            }
-            if let changed = changes.changedIndexes, changed.count > 0 {
-                collectionView.reloadItems(at: changed.map { IndexPath(item: $0, section:0) })
-            }
-            changes.enumerateMoves { fromIndex, toIndex in
-                collectionView.moveItem(at: IndexPath(item: fromIndex, section: 0),
-                                        to: IndexPath(item: toIndex, section: 0))
-            }
-        })
+    private func update(with changes: PHFetchResultChangeDetails<PHAsset>) {
+        if let removed = changes.removedIndexes, removed.count > 0 {
+            let paths = removed.map { IndexPath(item: $0, section: 0)}
+            let userInfo = [UserInfoKey.removedPaths: paths]
+            NotificationCenter.default.post(name: .photoDidDeleted,
+                                            object: self,
+                                            userInfo: userInfo)
+        }
+        if let inserted = changes.insertedIndexes, inserted.count > 0 {
+            let paths = inserted.map { IndexPath(item: $0, section: 0)}
+            let userInfo = [UserInfoKey.insertedPaths: paths]
+            NotificationCenter.default.post(name: .photoDidInserted,
+                                            object: self,
+                                            userInfo: userInfo)
+        }
+        if let changed = changes.changedIndexes, changed.count > 0 {
+            let paths = changed.map { IndexPath(item: $0, section: 0)}
+            let userInfo = [UserInfoKey.changedPaths: paths]
+            NotificationCenter.default.post(name: .photoDidChanged,
+                                            object: self,
+                                            userInfo: userInfo)
+        }
+        
+        changes.enumerateMoves { fromIndex, toIndex in
+            let userInfo = [UserInfoKey.movedPaths: (fromIndex, toIndex)]
+            NotificationCenter.default.post(name: .photoDidMoved,
+                                            object: self,
+                                            userInfo: userInfo)
+        }
     }
+}
+
+extension NSNotification.Name {
+    static let photoDidDeleted = NSNotification.Name("photoDidDeleted")
+    static let photoDidInserted = NSNotification.Name("photoDidInserted")
+    static let photoDidChanged = NSNotification.Name("photoDidChanged")
+    static let photoDidMoved = NSNotification.Name("photoDidMoved")
+    static let notIncrementalChanges = NSNotification.Name("notIncrementalChanges")
 }
