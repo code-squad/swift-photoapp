2bb871b878c3648c9aae9673e53121689f892c5b
Hngfu <djsl1018@naver.com>
2019-06-20 21:45:13 +0900

    [DoodleCollectionViewCell] 추가, 이미지뷰 추가

---
+++ PhotosApp/PhotosApp/Views/DoodleCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  DoodleCollectionViewCell.swift
+//  PhotosApp
+//
+//  Created by 조재흥 on 19. 6. 20..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import UIKit
+
+class DoodleCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    static let identifier = "doodleCollectionViewCell"
+    
+}
