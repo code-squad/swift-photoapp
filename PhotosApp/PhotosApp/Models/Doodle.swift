af3511fda3532080ee743389717da7faec60460b
Hngfu <djsl1018@naver.com>
2019-06-20 21:48:26 +0900

    [Doodle] 추가

---
+++ PhotosApp/PhotosApp/Models/Doodle.swift
@@ -0,0 +1,15 @@
+//
+//  Doodle.swift
+//  PhotosApp
+//
+//  Created by 조재흥 on 19. 6. 20..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import Foundation
+
+struct Doodle: Decodable {
+    let title: String
+    let image: String
+    let date: String
+}
