e32d9372c4a955168e0b4076778e9b198d7f881c
Hngfu <djsl1018@naver.com>
2019-06-20 22:23:44 +0900

    [NetworkManager] 추가, [DoodleManager] 추가

---
+++ PhotosApp/PhotosApp/NetworkManager.swift
@@ -0,0 +1,25 @@
+//
+//  NetworkManager.swift
+//  PhotosApp
+//
+//  Created by 조재흥 on 19. 6. 20..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    func download(with url: URL, successHandler: @escaping (Data) -> Void) {
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if error != nil {
+                return
+            }
+            
+            guard let data = data,
+                let response = response as? HTTPURLResponse,
+                200...299 ~= response.statusCode else { return }
+            
+            successHandler(data)
+        }.resume()
+    }
+}
